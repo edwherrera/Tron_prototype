4dd51d02f9e437be78939f2e4119fa7fca66caed
Edwin Herrera <edwherrera.175@gmail.com>
2016-09-23 21:58:02 -0600

    Delete console.py

--- Console/console.py
+++
@@ -1,9 +0,0 @@
-import Logic
-
-from operator import itemgetter
-
-
-class Console:
-    def __init__(self):
-        parsed = Logic.parser.parse_action_file('actions.json')
-        self.board = Logic.board.Board(action_list, players)
